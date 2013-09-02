0de928e61c85cf0440497d8a8bce46cc55175c0b
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-02 09:28:59 +0000

    fixed copyright notice
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/lti/taoLti/trunk@15681 62dfeb86-a93f-4914-9d13-15c0a160985d

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-/*  
+/**
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -14,16 +15,12 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
- * Copyright (c) 2013 (original work) Open Assessment Techonologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *               
  * 
  */
 
-/*
- * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
- * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
- */
-require_once dirname(__FILE__). '/../tao/includes/class.Bootstrap.php';
+require_once dirname(__FILE__) . '/../tao/includes/class.Bootstrap.php';
 
 $bootStrap = new BootStrap('taoLti');
 $bootStrap->start();
