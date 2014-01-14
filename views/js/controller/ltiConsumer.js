7d282fce1901585597243e54eda66e3e2ca07da8
bertrand <bertrand@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-01-14 10:31:30 +0000

    Fix frame sizing and check for cross origin
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/lti/taoLti/trunk@17837 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/controller/ltiConsumer.js
+++ views/js/controller/ltiConsumer.js
@@ -10,6 +10,9 @@ define(['jquery', 'iframeNotifier'], function($, iframeNotifier){
              
             //set a fix size to the iframe as we are not allowed to check content size
             iframeNotifier.parent('heightchange', [600]);
+            
+            //ask the parent to stop communicate accross frames because we are going to another domain for new amazing adventures...
+            iframeNotifier.parent('shutdown-com');
         }
     };
 });
