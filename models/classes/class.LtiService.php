b7fefc39f899dae37dcb01870a226bc6a588d4b9
Joel Bout <joel@taotesting.com>
2014-09-05 11:45:59 +0200

    removed deprecated core_kernel_classes_Session call

--- models/classes/class.LtiService.php
+++ models/classes/class.LtiService.php
@@ -43,7 +43,7 @@ class taoLti_models_classes_LtiService extends tao_models_classes_Service
         $adapter = new taoLti_models_classes_LtiAuthAdapter($request);
         $user = $adapter->authenticate();
         $session = new taoLti_models_classes_TaoLtiSession($user);
-        core_kernel_classes_Session::singleton()->setSession($session);
+        common_session_SessionManager::startSession($session);
 	}
 	
 	/**
