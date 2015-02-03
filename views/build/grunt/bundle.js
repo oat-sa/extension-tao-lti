512ba2c6cb873bc2a52762ba24c4c4d501aaa9b7
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-02-03 11:23:55 +0100

    fix parrallel build

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
     /**
      * Remove bundled and bundling files
      */
-    clean.taoltibundle = [out,  root + '/taoLti/views/js/controllers.min.js'];
+    clean.taoltibundle = [out];
     
     /**
      * Compile tao files into a bundle 
