113e94407a2f281b3f5cf65f90ce6672bfc28af9
siwane <camille@taotesting.com>
2018-07-20 11:01:59 +0200

    Do not rawurlencode twice by calling _url only once

--- controller/ToolModule.php
+++ controller/ToolModule.php
@@ -63,17 +63,11 @@ abstract class ToolModule extends LtiModule
             $cookieService = $this->getServiceManager()->get(CookieVerifyService::SERVICE_ID);
             if ($cookieService->isVerifyCookieRequired()) {
                 if (tao_models_classes_accessControl_AclProxy::hasAccess('verifyCookie', 'CookieUtils', 'taoLti')) {
-                    $this->redirect(
-                        _url(
-                            'verifyCookie',
-                            'CookieUtils',
-                            'taoLti',
-                            [
-                                'session'  => session_id(),
-                                'redirect' => _url('run', null, null, $_GET)
-                            ]
-                        )
-                    );
+                    $cookieRedirect = \tao_helpers_Uri::getRootUrl() .
+                        'taoLti/CookieUtils/verifyCookie' .
+                        '?session=' . session_id() .
+                        '&redirect=' . _url('run', null, null, $_GET);
+                    $this->redirect($cookieRedirect);
                 } else {
                     throw new LtiException(
                         __('You are not authorized to use this system'),
