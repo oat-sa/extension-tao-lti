a1208993797e379baef8637dcc14a17de3a9c23a
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-10-05 11:46:53 +0200

    migrate to atomic bundles

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -1,50 +1,43 @@
-module.exports = function(grunt) { 
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2018 (original work) Open Assessment Technologies SA;
+ */
 
-    var requirejs   = grunt.config('requirejs') || {};
-    var clean       = grunt.config('clean') || {};
-    var copy        = grunt.config('copy') || {};
+/**
+ * configure the extension bundles
+ * @author Bertrand Chevrier <bertrand@taotesting.com>
+ */
+module.exports = function(grunt) {
+    'use strict';
 
-    var root        = grunt.option('root');
-    var libs        = grunt.option('mainlibs');
-    var ext         = require(root + '/tao/views/build/tasks/helpers/extensions')(grunt, root);
-    var out         = 'output';
-
-    /**
-     * Remove bundled and bundling files
-     */
-    clean.taoltibundle = [out];
-    
-    /**
-     * Compile tao files into a bundle 
-     */
-    requirejs.taoltibundle = {
-        options: {
-            baseUrl : '../js',
-            dir : out,
-            mainConfigFile : './config/requirejs.build.js',
-            paths : { 'taoLti' : root + '/taoLti/views/js' },
-            modules : [{
-                name: 'taoLti/controller/routes',
-                include : ext.getExtensionsControllers(['taoLti']),
-                exclude : ['mathJax'].concat(libs)
-            }]
+    grunt.config.merge({
+        bundle : {
+            taolti : {
+                options : {
+                    extension : 'taoLti',
+                    outputDir : 'loader',
+                    bundles : [{
+                        name : 'taoLti',
+                        default : true
+                    }]
+                }
+            }
         }
-    };
-
-    /**
-     * copy the bundles to the right place
-     */
-    copy.taoltibundle = {
-        files: [
-            { src: [out + '/taoLti/controller/routes.js'],  dest: root + '/taoLti/views/js/controllers.min.js' },
-            { src: [out + '/taoLti/controller/routes.js.map'],  dest: root + '/taoLti/views/js/controllers.min.js.map' }
-        ]
-    };
-
-    grunt.config('clean', clean);
-    grunt.config('requirejs', requirejs);
-    grunt.config('copy', copy);
+    });
 
     // bundle task
-    grunt.registerTask('taoltibundle', ['clean:taoltibundle', 'requirejs:taoltibundle', 'copy:taoltibundle']);
+    grunt.registerTask('taoltibundle', ['bundle:taolti']);
 };
