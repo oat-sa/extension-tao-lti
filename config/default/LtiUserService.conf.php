5227a47d877ea3106c17c45c5926aa9392c790ed
Aleh Hutnikau <hutnikau@1pt.com>
2019-09-12 16:50:59 +0300

    add factoryLtiUser to default config of LtiUserService

--- config/default/LtiUserService.conf.php
+++ config/default/LtiUserService.conf.php
@@ -1,2 +1,6 @@
 <?php
-return new \oat\taoLti\models\classes\user\OntologyLtiUserService([]);
+use oat\taoLti\models\classes\user\OntologyLtiUserService;
+
+return new OntologyLtiUserService([
+    OntologyLtiUserService::OPTION_FACTORY_LTI_USER => 'taoLti/LtiUserFactory'
+]);
