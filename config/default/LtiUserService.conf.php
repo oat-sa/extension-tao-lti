23384e84c9d2ccec54b90e40fdb89c80d2fa0e93
Julien Sébire <julien@taotesting.com>
2019-09-12 16:00:14 +0200

    Fixed config array.

--- config/default/LtiUserService.conf.php
+++ config/default/LtiUserService.conf.php
@@ -3,5 +3,5 @@ use oat\taoLti\models\classes\user\LtiUserFactoryService;
 use oat\taoLti\models\classes\user\LtiUserService;
 
 return new \oat\taoLti\models\classes\user\OntologyLtiUserService([
-    LtiUserService::OPTION_FACTORY_LTI_USER, LtiUserFactoryService::SERVICE_ID,
+    LtiUserService::OPTION_FACTORY_LTI_USER => LtiUserFactoryService::SERVICE_ID,
 ]);
