71fb13370015df2b80d01444772296173adba0d1
Razhkou <yuryr@taotesting.com>
2019-10-10 12:30:51 +0300

    TTD-18 Added\changed 'generis' version to '>=12.5.0' in `requires` secition

--- manifest.php
+++ manifest.php
@@ -40,7 +40,7 @@ return array(
     'version' => '10.8.0',
       'author' => 'Open Assessment Technologies SA',
       'requires' => array(
-        'generis' => '>=12.1.0',
+        'generis' => '>=12.5.0',
         'tao' => '>=31.8.0'
     ),
     'routes' => array(
