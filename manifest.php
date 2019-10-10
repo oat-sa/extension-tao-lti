0be5b5c31607eb7a150adfe37c3db3c1aa01d2c7
Razhkou <yuryr@taotesting.com>
2019-10-10 19:18:17 +0300

    TTD-18 version bump

--- manifest.php
+++ manifest.php
@@ -37,7 +37,7 @@ return array(
     'label' => 'LTI library',
     'description' => 'TAO LTI library and helpers',
     'license' => 'GPL-2.0',
-    'version' => '10.8.0',
+    'version' => '10.9.0',
       'author' => 'Open Assessment Technologies SA',
       'requires' => array(
         'generis' => '>=12.5.0',
