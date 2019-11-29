a135c5fbd54ccb7958676b0cf7cae856acb0b8cd
Bout <joel@taotesting.com>
2019-11-29 11:35:02 +0100

    Fix automerge

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -251,7 +251,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('10.5.3');
         }
 
-        $this->skip('10.5.3', '10.10.0');
+        $this->skip('10.5.3', '10.9.1');
 
         if ($this->isVersion('10.9.1')) {
             $this->getServiceManager()->register(LisOauthService::SERVICE_ID,
