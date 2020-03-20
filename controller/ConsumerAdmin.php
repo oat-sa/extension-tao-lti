5980a24aaedb09d392fd8c5916b10e98d9477d31
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2020-03-20 12:45:19 +0100

    TBD-13 feat: remove resource ID from LTI consumer edit form

--- controller/ConsumerAdmin.php
+++ controller/ConsumerAdmin.php
@@ -20,14 +20,18 @@
 
 namespace oat\taoLti\controller;
 
-use common_exception_BadRequest as BadRequestExcetpion;
+use common_exception_BadRequest as BadRequestException;
 use core_kernel_classes_Class as KernelClass;
+use core_kernel_classes_Resource as KernelResource;
+use oat\tao\model\controller\SignedFormInstance;
 use oat\tao\model\oauth\DataStore;
 use oat\taoLti\models\classes\ConsumerService;
 use oat\taoLti\models\classes\Security\Business\Contract\SecretKeyServiceInterface;
 use tao_actions_form_CreateInstance as CreateInstanceContainer;
 use tao_actions_SaSModule;
 use tao_helpers_form_Form as Form;
+use tao_models_classes_dataBinding_GenerisFormDataBinder;
+use tao_models_classes_dataBinding_GenerisFormDataBindingException as FormDataBindingException;
 
 /**
  * This controller allows the additon and deletion
@@ -41,19 +45,40 @@ class ConsumerAdmin extends tao_actions_SaSModule
     /** @var KernelClass */
     private $currentClass;
 
+    private $currentInstance;
+
+    /**
+     * @inheritDoc
+     */
     public function addInstanceForm(): void
     {
         if (!$this->isXmlHttpRequest()) {
-            throw new BadRequestExcetpion('wrong request mode');
+            throw new BadRequestException('wrong request mode');
         }
 
-        $addInstanceForm = $this->createNewInstanceContainer();
+        $form = $this->createNewInstanceForm();
 
-        $this->handleNewInstanceSubmission($addInstanceForm);
+        $this->handleNewInstanceSubmission($form);
 
         $this->setData('formTitle', __('Create instance of ') . $this->getCurrentClass()->getLabel());
-        $this->setData('myForm', $addInstanceForm->render());
+        $this->setData('myForm', $form->render());
 
+        $this->setView('form.tpl', 'tao');
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @throws FormDataBindingException
+     */
+    public function editInstance(): void
+    {
+        $form = $this->createExistingInstanceForm();
+
+        $this->handleExistingInstanceSubmission($form);
+
+        $this->setData('formTitle', __('Edit Instance'));
+        $this->setData('myForm', $form->render());
         $this->setView('form.tpl', 'tao');
     }
 
@@ -65,6 +90,9 @@ class ConsumerAdmin extends tao_actions_SaSModule
         return $this->getServiceLocator()->get(ConsumerService::class);
     }
 
+    /**
+     * @inheritDoc
+     */
     protected function getCurrentClass(): KernelClass
     {
         if (null === $this->currentClass) {
@@ -74,7 +102,24 @@ class ConsumerAdmin extends tao_actions_SaSModule
         return $this->currentClass;
     }
 
-    private function createNewInstanceContainer(): Form
+    /**
+     * @noinspection PhpDocMissingThrowsInspection
+     *
+     * @param string $parameterName
+     *
+     * @return KernelResource
+     */
+    protected function getCurrentInstance($parameterName = 'uri'): KernelResource
+    {
+        if (null === $this->currentInstance) {
+            /** @noinspection PhpUnhandledExceptionInspection */
+            $this->currentInstance = parent::getCurrentInstance($parameterName);
+        }
+
+        return $this->currentInstance;
+    }
+
+    private function createNewInstanceForm(): Form
     {
         $formContainer = new CreateInstanceContainer(
             [$this->getCurrentClass()],
@@ -85,6 +130,20 @@ class ConsumerAdmin extends tao_actions_SaSModule
         );
 
         return $formContainer->getForm();
+    }
+
+    private function createExistingInstanceForm(): Form
+    {
+        $myFormContainer = new SignedFormInstance(
+            $this->getCurrentClass(),
+            $this->getCurrentInstance(),
+            [SignedFormInstance::CSRF_PROTECTION_OPTION => true]
+        );
+
+        $form = $myFormContainer->getForm();
+        $form->removeElement('id');
+
+        return $form;
     }
 
     /**
@@ -99,6 +158,24 @@ class ConsumerAdmin extends tao_actions_SaSModule
             $instance = $this->createInstance([$this->getCurrentClass()], $properties);
 
             $this->setData('message', __('%s created', $instance->getLabel()));
+            $this->setData('reload', true);
+        }
+    }
+
+    /**
+     * @param Form $form
+     *
+     * @throws FormDataBindingException
+     */
+    private function handleExistingInstanceSubmission(Form $form): void
+    {
+        if ($form->isSubmited() && $form->isValid()) {
+            $values = $form->getValues();
+
+            $binder = new tao_models_classes_dataBinding_GenerisFormDataBinder($this->getCurrentInstance());
+            $binder->bind($values);
+
+            $this->setData('message', __('Instance saved'));
             $this->setData('reload', true);
         }
     }
