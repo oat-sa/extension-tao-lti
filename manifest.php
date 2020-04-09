bc715b0d52ff0b8198293e31af395aeaeaf1b54d
Aleh Hutnikau <aleh.hutnikau@1pt.com>
2020-04-09 12:09:17 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -37,7 +37,7 @@ return [
     'label' => 'LTI library',
     'description' => 'TAO LTI library and helpers',
     'license' => 'GPL-2.0',
-    'version' => '11.7.0',
+    'version' => '11.7.1',
       'author' => 'Open Assessment Technologies SA',
       'requires' => [
         'generis' => '>=12.15.0',
