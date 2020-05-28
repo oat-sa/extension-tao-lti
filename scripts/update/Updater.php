22eb51bc5c0f1009ca9a798ff280def326f0989b
Joel Bout <joel@taotesting.com>
2020-05-28 07:22:16 +0200

    Bump v11.7.3

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -278,6 +278,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('11.6.0');
         }
 
-        $this->skip('11.6.0', '11.7.2');
+        $this->skip('11.6.0', '11.7.3');
     }
 }
