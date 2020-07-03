6e07e7c70ec1654e7672cbc8b20784a97d771a25
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-07-03 12:07:42 +0200

    version bump

--- manifest.php
+++ manifest.php
@@ -35,7 +35,7 @@ return [
     'label' => 'LTI library',
     'description' => 'TAO LTI library and helpers',
     'license' => 'GPL-2.0',
-    'version' => '11.10.1',
+    'version' => '11.10.2',
       'author' => 'Open Assessment Technologies SA',
       'requires' => [
         'generis' => '>=12.15.0',
