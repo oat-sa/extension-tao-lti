c64703176244491d6c784260cfa292650744c8fc
Mikhail Kamarouski <Mikhail.Kamarouski@1pt.com>
2020-09-17 13:40:30 +0300

    Specifies exact order for the LTI form fields

---
+++ migrations/Version202009171027563772_taoLti.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoLti\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\tao\scripts\update\OntologyUpdater;
+
+final class Version202009171027563772_taoLti extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        OntologyUpdater::syncModels();
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
