764ea6bfd6242ecf830977b05c90d2042e043af8
Mikhail Kamarouski <Mikhail.Kamarouski@1pt.com>
2020-09-23 17:50:27 +0300

    PSR-12

--- test/unit/models/classes/LtiProvider/LtiProviderValidatorTest.php
+++ test/unit/models/classes/LtiProvider/LtiProviderValidatorTest.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2016-2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
 declare(strict_types=1);
@@ -69,6 +69,4 @@ class LtiProviderValidatorTest extends TestCase
             )
         );
     }
-
-
 }
