7944ca692ccacbbee58d790a96be2f72e74f4125
Camille Moyon <camille@taotesting.com>
2020-09-24 13:53:43 +0200

    Remove unnecessary change

--- models/classes/Security/AuthorizationServer/AuthorizationServerFactory.php
+++ models/classes/Security/AuthorizationServer/AuthorizationServerFactory.php
@@ -48,7 +48,7 @@ class AuthorizationServerFactory extends ConfigurableService
 
     public function create(KeyChain $keyChain): AuthorizationServer
     {
-        if (null == $keyChain->getPrivateKey()) {
+        if (null === $keyChain->getPrivateKey()) {
             throw new InvalidArgumentException('Missing private key');
         }
 
