26e815eec304f01ffcf3d9059bdc399514281306
Bartlomiej Marszal <bartlomiej@taotesting.com>
2020-09-25 08:15:48 +0200

    Migration Version202009241529173772

--- migrations/Version202009241529173772_taoLti.php
+++ migrations/Version202009241529173772_taoLti.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace oat\taoLti\migrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use oat\tao\model\menu\SectionVisibilityFilter;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
 use oat\taoLti\models\classes\LtiProvider\FeatureFlagFormPropertyMapper;
+use oat\taoLti\models\classes\LtiProvider\RdfLtiProviderRepository;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -16,7 +18,7 @@ final class Version202009241529173772_taoLti extends AbstractMigration
 
     public function getDescription(): string
     {
-        return '';
+        return 'Disable LTI 1.3 section and LtiProvider form fields base on LTI1P3 Feature flag';
     }
 
     public function up(Schema $schema): void
@@ -25,11 +27,57 @@ final class Version202009241529173772_taoLti extends AbstractMigration
             FeatureFlagFormPropertyMapper::SERVICE_ID,
             new FeatureFlagFormPropertyMapper(
                 [
-                    FeatureFlagFormPropertyMapper::OPTION_FEATURE_FLAG_FORM_FIELDS => [],
+                    FeatureFlagFormPropertyMapper::OPTION_FEATURE_FLAG_FORM_FIELDS => [
+                        RdfLtiProviderRepository::LTI_TOOL_IDENTIFIER => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_PUBLIC_KEY => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_JWKS_URL => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_LAUNCH_URL => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_OIDC_LOGIN_INITATION_URL => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_DEPLOYMENT_IDS => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_AUDIENCE => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_CLIENT_ID => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_NAME => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_TOOL_IDENTIFIER => [
+                            'LTI1P3'
+                        ],
+                        RdfLtiProviderRepository::LTI_VERSION => [
+                            'LTI1P3'
+                        ],
+                    ]
                 ]
             )
         );
 
+        $this->getServiceManager()->register(
+            SectionVisibilityFilter::SERVICE_ID,
+            new SectionVisibilityFilter(
+                [
+                    SectionVisibilityFilter::OPTION_FEATURE_FLAG_SECTIONS => [
+                        'settings_manage_lti_keys' => [
+                            'LTI1P3'
+                        ]
+                    ]
+                ]
+            )
+        );
     }
 
     public function down(Schema $schema): void
