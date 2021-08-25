f1dd0b09695478b4ea257e514dae080c2e58b844
Chinnu Francis <chinnu@focaloid.com>
2021-08-25 15:03:29 +0530

    refactor: use common function defined in searchProxy

--- migrations/Version202108190925463772_taoLti.php
+++ migrations/Version202108190925463772_taoLti.php
@@ -20,40 +20,26 @@ final class Version202108190925463772_taoLti extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $generisSearchWhitelist = $this->getLTIClassURI();
         $searchProxy = $this->getProxy();
-        
-        if ($searchProxy->hasOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST)) {
-            $options = $searchProxy->getOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST);
-            $generisSearchWhitelist = array_merge($options, $generisSearchWhitelist);
-        }
-        $searchProxy->setOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST, $generisSearchWhitelist);
-        
+        $searchProxy->extendGenerisSearchWhiteList([
+            ConsumerService::CLASS_URI,
+            ProviderService::CLASS_URI,
+        ]);
         $this->getServiceManager()->register(SearchProxy::SERVICE_ID, $searchProxy);
     }
 
     public function down(Schema $schema): void
     {
-        $generisSearchWhitelist = $this->getLTIClassURI();
         $searchProxy = $this->getProxy();
-        if ($searchProxy->hasOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST)) {
-            $options = $searchProxy->getOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST);
-            $generisSearchlist = array_diff($options, $generisSearchWhitelist);
-            $searchProxy->setOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST, $generisSearchlist);
-        }
+        $searchProxy->removeFromGenerisSearchWhiteList([
+            ConsumerService::CLASS_URI,
+            ProviderService::CLASS_URI,
+        ]);
         $this->getServiceManager()->register(SearchProxy::SERVICE_ID, $searchProxy);
     }
 
     private function getProxy(): SearchProxy
     {
         return $this->getServiceManager()->get(SearchProxy::SERVICE_ID);
-    }
-
-    private function getLTIClassURI(): array
-    {
-        return [
-            ConsumerService::CLASS_URI,
-            ProviderService::CLASS_URI,
-        ];
     }
 }
--- scripts/install/GenerisSearchWhitelist.php
+++ scripts/install/GenerisSearchWhitelist.php
@@ -33,17 +33,10 @@ class GenerisSearchWhitelist extends InstallAction
     {
         /** @var SearchProxy $searchProxy */
         $searchProxy = $this->getServiceManager()->get(SearchProxy::SERVICE_ID);
-
-        $generisSearchWhitelist = [
+        $searchProxy->extendGenerisSearchWhiteList([
             ConsumerService::CLASS_URI,
             ProviderService::CLASS_URI,
-        ];
-
-        if ($searchProxy->hasOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST)) {
-            $options = $searchProxy->getOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST);
-            $generisSearchWhitelist = array_merge($options, $generisSearchWhitelist);
-        }
-        $searchProxy->setOption(SearchProxy::OPTION_GENERIS_SEARCH_WHITELIST, $generisSearchWhitelist);
+        ]);
 
         $this->getServiceManager()->register(SearchProxy::SERVICE_ID, $searchProxy);
     }
