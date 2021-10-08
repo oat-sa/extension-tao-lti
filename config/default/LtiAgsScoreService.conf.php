ffeb5773d39d1f11b0bd244def9bb802b6174f7d
Eugene Golubev <emgolubev@gmail.com>
2021-10-08 12:18:37 +0300

    fix: broken taoSetup

--- config/default/LtiAgsScoreService.conf.php
+++ config/default/LtiAgsScoreService.conf.php
@@ -1,12 +1,5 @@
 <?php
 
-use OAT\Library\Lti1p3Ags\Factory\Score\ScoreFactory;
-use OAT\Library\Lti1p3Ags\Service\Score\Client\ScoreServiceClient;
 use oat\taoLti\models\classes\LtiAgsScoreService;
 
-return new LtiAgsScoreService(
-    [
-        LtiAgsScoreService::OPTION_SCORE_SERVICE_CLIENT => new ScoreServiceClient(),
-        LtiAgsScoreService::OPTION_SCORE_FACTORY => new ScoreFactory(),
-    ]
-);
+return new LtiAgsScoreService();
