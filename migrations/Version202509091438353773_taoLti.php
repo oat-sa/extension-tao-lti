98be40b5ff4e2e77811300f55230ddf017e02f3b
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2025-09-11 15:03:48 +0200

    fix: migration

--- migrations/Version202509091438353773_taoLti.php
+++ migrations/Version202509091438353773_taoLti.php
@@ -6,28 +6,27 @@ namespace oat\taoLti\migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\taoLti\scripts\install\RegisterLtiAclRoleProvider;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  *
  * phpcs:disable Squiz.Classes.ValidClassName
  */
-final class Version202509091438353772_taoLti extends AbstractMigration
+final class Version202509091438353773_taoLti extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'This migration registers the LTI ACL role provider.';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-
+        $this->runAction(new RegisterLtiAclRoleProvider());
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $this->throwIrreversibleMigrationException();
     }
 }
